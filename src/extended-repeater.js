96c8d26394f919e5cd9bf8ace1c02654081f0f19
stroich <stroichmwork@gmail.com>
2023-04-15 18:29:29 +0400

    feat: add js repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  let addition = '';
+  let result =''
+  if('addition' in options){
+    if ('additionSeparator' in options){
+      addition = String(options.addition)
+      if('additionRepeatTimes' in options){
+        addition = String(options.addition)+options.additionSeparator;
+        addition= addition.repeat(options.additionRepeatTimes-1)+String(options.addition);
+      }
+    }else {
+      addition = String(options.addition)
+      if('additionRepeatTimes' in options){
+        addition = String(options.addition)+'|';
+        addition= addition.repeat(options.additionRepeatTimes-1)+String(options.addition);
+      }
+    }
+  }
+  console.log(addition)
+  if('separator' in options){
+    result = String(str)+String(addition);
+    console.log(result)
+    if('repeatTimes' in options){
+      result =result+options.separator;
+      result= result.repeat(options.repeatTimes-1)+String(str)+String(addition);
+    }
+  }else {
+    result = String(str)+String(addition);
+    if('repeatTimes' in options){
+      result =String(str)+String(addition)+'+';
+      result= result.repeat(options.repeatTimes-1)+String(str)+String(addition);
+    }
+  }
+  return result;
 }
 
 module.exports = {
